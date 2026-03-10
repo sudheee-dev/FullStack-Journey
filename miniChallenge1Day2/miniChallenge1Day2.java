c6b2d39ad33e3c0559cc21598b54a10c436d6d5e
sudheer Sub <sudheerbhagavatula@gmail.com>
2026-03-11 01:31:08 +0530

    min

---
+++ miniChallenge1Day2/miniChallenge1Day2.java
@@ -0,0 +1,14 @@
+package miniChallenge1Day2;
+
+public class miniChallenge1Day2 {
+    public static void main(String[] args) {
+        String name = "Sudheer";
+        int salary = 100000;
+        String city = "kakinada";
+        double rating = 9.5;
+        System.out.println("Name :" +name);
+        System.out.println("City :" +city);
+        System.out.println("Salary :" +salary);
+        System.out.println("Rating :" +rating);
+    }
+}
